53d14d158f9878b5ee69db097b1a47eac4418295
Joshua Morgan <jmorg0605@gmail.com>
2024-09-04 12:05:49 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="_" target="_blank">
+          <img src="clothing.svg" className="logo" alt="clothing logo" />
+        </a>
+      </div>
+    </>
+  );
+}
+
+export default App;
